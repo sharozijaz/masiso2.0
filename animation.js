bea8c5eca590f264bf4a8ce09f985859e9b7b84c
Sharoz Ijaz <sherry@sharoz.dev>
2022-07-05 19:53:24 +0500

    animation

--- animation.js
+++ animation.js
@@ -1,9 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
 var tl = gsap.timeline();
+
 tl.from(".animate__hero", { duration: 2, x: 900, ease: "power4.out" });
-gsap.to(".centre_mobile", {
-  duration: 0.5,
-  scale: 1.1,
-  ease: "power1.inOut",
-  repeat: 1,
-  yoyo: true,
+
+tl.from(".left", {
+  duration: 1,
+  x: -300,
+  ease: "power1.out",
+  stagger: 0.25,
+});
+tl.from(".right", {
+  duration: 1,
+  x: 300,
+  ease: "power1.out",
+  stagger: 0.25,
 });
