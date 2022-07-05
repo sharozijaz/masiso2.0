00e69dbdda431b5c5f7192c867f21dbb50437194
Sharoz Ijaz <sherry@sharoz.dev>
2022-07-05 23:34:57 +0500

    stag

--- animation.js
+++ animation.js
@@ -3,13 +3,13 @@ var tl = gsap.timeline();
 
 tl.from(".animate__hero", { duration: 2, x: 900, ease: "power4.out" });
 
-tl.from(".left", {
+gsap.from(".left", {
   duration: 1,
   x: -300,
   ease: "power1.out",
   stagger: 0.25,
 });
-tl.from(".right", {
+gsap.from(".right", {
   duration: 1,
   x: 300,
   ease: "power1.out",
