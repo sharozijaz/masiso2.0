f04c5e24af321f07b9cf69d0c742b9116f69da5d
Sharoz Ijaz <sherry@sharoz.dev>
2022-07-05 17:44:36 +0500

    animate

--- animation.js
+++ animation.js
@@ -1,2 +1,9 @@
-var tl = gsap.timeline({ duration: 1, ease: "power1.out" });
-tl.from(".animate__hero", { duration: 3, x: 400, ease: "power4.out" });
+var tl = gsap.timeline();
+tl.from(".animate__hero", { duration: 2, x: 900, ease: "power4.out" });
+gsap.to(".centre_mobile", {
+  duration: 0.5,
+  scale: 1.1,
+  ease: "power1.inOut",
+  repeat: 1,
+  yoyo: true,
+});
